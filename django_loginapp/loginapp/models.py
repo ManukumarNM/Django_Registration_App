e1633dac66bea6f02813e04affb970a54ab06877
ManukumarNM <manunm.jps@gmail.com>
2021-06-16 22:08:55 +0530

    First commit Message

---
+++ django_loginapp/loginapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# class Signup(models.Model):
+#     username = models.CharField(max_length=250)
+#     email = models.EmailField()
+#     password = mod
---
+++ django_loginapp/loginapp/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect 
+from django.contrib.auth.models import User, auth 
+from django.contrib import messages 
+# from . models import Signup
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Alreday Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                messages.info(request, "User Created Successfully")
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('register')
+
+        return redirect('/')
+
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Credentials')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+    
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
